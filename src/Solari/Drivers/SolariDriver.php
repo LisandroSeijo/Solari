a782b2a3dc4f4ddcb0f49e949b7fafc55dc162bd
LisandroSeijo <lisandro_cuervo@hotmail.com>
2014-04-06 02:31:19 -0300

    Add toJson() method in SolariDriver class

--- src/Solari/Drivers/SolariDriver.php
+++ src/Solari/Drivers/SolariDriver.php
@@ -5,8 +5,38 @@ namespace Solari\Drivers;
 
 abstract class SolariDriver 
 {
+	/**
+	* Iframe code to embed
+	*/
 	abstract protected function embed();
+
+	/**
+	* Return title
+	*/
 	abstract protected function title();
+
+	/**
+	* Return description
+	*/
 	abstract protected function description();
+
+	/**
+	* Return image
+	*/
 	abstract protected function img();
+
+	/**
+	* Return data in json
+	*/
+	public function toJson()
+	{
+		return json_encode(
+			array(
+				'title'       => $this->title(),
+				'description' => $this->description(),
+				'img'         => $this->img(),
+				'embed'       => $this->embed()
+			)
+		);
+	}
 }
